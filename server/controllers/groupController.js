8392ed09f6c362c25f586b48f4c0c38750ad6f08
ricin9 <ricin9@outlook.com>
2022-05-14 19:24:02 +0100

    fixed group controller, added validation to user model

--- server/controllers/groupController.js
+++ server/controllers/groupController.js
@@ -1,119 +1,144 @@
-const asyncHandler = require('express-async-handler')
-const Group = require('../models/groupModel')
+const asyncHandler = require("express-async-handler")
+const Group = require("../models/groupModel")
 
 const getGroups = asyncHandler(async (req, res) => {
-  const groups = await Group.find({name : {$ne : 'admin'}}).select('name path').sort({path : -1})
+	const groups = await Group.find({ name: { $ne: "admin" } })
+		.select("name path")
+		.sort({ path: -1 })
 
-  if (groups.length === 0) {
-    res.status(404)
-    throw new Error('no groups found')
-  }
+	if (groups.length === 0) {
+		res.status(404)
+		throw new Error("no groups found")
+	}
 
-  const trasnformed = groupTransform(groups)
-  res.status(200).json(trasnformed)
+	const trasnformed = groupTransform(groups)
+	res.status(200).json(trasnformed)
 })
 
-
 const createGroup = asyncHandler(async (req, res) => {
+	if (!req.user.isAdmin) {
+		res.status(401)
+		throw new Error("Only admins are allowed to create groups")
+	}
 
-  // verify if group already exists
-  if (await Group.findOne(req.body)) {
-    res.status(400)
-    throw new Error('group already exists')
-  }
+	const { name, parent: parentId } = req.body
 
-  // verify if parent exists
-  // unfinished
-  let groupObj
-  if (req.body.parent) {
-    const parent = Group.findById(req.body.parent)
-    if (!parent) {
-      res.status(404)
-      throw new Error('Group parent doesn\'t exist')
-    } 
+	if (parent) {
+		//search for parent
+		const parent = await Group.findById(parentId)
 
-    groupObj = new Group({name : req.body.name, path : parent.path + parent.name + '/'})
-    
-  } else {
-    groupObj = new Group({name : req.body.name, path : '/'})
-  }
+		if (!parent) {
+			res.status(404)
+			throw new Error("Parent group doesn't exist")
+		}
 
-  // create and save group
-  await groupObj.save()
-  res.status(201).json(groupObj)
+		const path = parent.path + parent.name + "/"
+		const group = await Group.findOne({ name, path })
+
+		if (group) {
+			res.status(400)
+			throw new Error("Group already exists")
+		}
+
+		try {
+			const newGroup = await Group.create({ name, path })
+
+			res.status(201).json(newGroup)
+		} catch (error) {
+			res.status(400)
+			throw new Error(error)
+		}
+	} else {
+		const group = await Group.findOne({ name, path: "/" })
+
+		if (group) {
+			res.status(400)
+			throw new Error("Group already exists")
+		}
+
+		try {
+			const newGroup = await Group.create({ name, path: "/" })
+
+			res.status(201).json(newGroup)
+		} catch (error) {
+			res.status(400)
+			throw new Error(error)
+		}
+	}
 })
 
 const getGroupsCustom = asyncHandler(async (req, res) => {
+	// search for group
+	const group = await Group.findOne({ name: req.params.name })
 
-  // search for group
-  const group = await Group.findOne({name : req.params.name})
+	if (!group) {
+		res.status(404)
+		throw new Error("group doesnt exist")
+	}
 
-  if (!group) {
-    res.status(404)
-    throw new Error('group doesnt exist')
-  }
+	let groups
+	// handle search query based on request
+	switch (req.query.type) {
+		case "allChildren": {
+			groups = await Group.find({
+				path: new RegExp(`${group.path}${group.name}/\w*`),
+			})
+			break
+		}
 
-  let groups;
-  // handle search query based on request
-  switch(req.query.type) {
-    case 'allChildren' : {
-      groups = await Group.find({path : new RegExp(`${group.path}${group.name}/\w*`)})
-      break
-    }
+		case "immediateChildren": {
+			groups = await Group.find({ path: `${group.path}${group.name}/` })
+			break
+		}
 
-    case 'immediateChildren' : {
-     groups = await Group.find({path :`${group.path}${group.name}/`})
-      break
-    }
+		case "parents": {
+			const parents = group.path.split("/")
+			groups = await Group.find({ name: { $in: parents } })
+			break
+		}
 
-    case 'parents' : {
-      const parents = group.path.split('/')
-      groups = await Group.find({name : {$in : parents}})
-      break
-    }
+		default: {
+			groups = group
+			break
+		}
+	}
 
-    default : {
-      groups = group
-      break
-    }
-  }
-
-  if (groups.length === 0) {
-    res.status(404)
-    throw new Error('no groups found')
-  }
-  res.status(200).json(groups)
+	if (groups.length === 0) {
+		res.status(404)
+		throw new Error("no groups found")
+	}
+	res.status(200).json(groups)
 })
 
 const groupTransform = (arr) => {
-  let childrenMap = new Map()
-  let path
-    while(arr.length !== 0) {
-        path = arr[0].path
-        let i = 0
-        let children = []
-        while(arr.length > i && arr[i].path === path) {
-          let child = {
-            _id : arr[i]._id,
-            name : arr[i].name
-          }
-  
-          if (childrenMap.has(`${path}${child.name}/`)) {
-            child.children = childrenMap.get(`${path}${child.name}/`)
-            childrenMap.delete(`${path}${child.name}/`)
-          }
-  
-          children.push(child)
-          i++
-        }
-        childrenMap.set(path, children)
-        arr.splice(0,i)
-  }
-  return childrenMap.get('/')
+	let childrenMap = new Map()
+	let path
+	while (arr.length !== 0) {
+		path = arr[0].path
+		let i = 0
+		let children = []
+		while (arr.length > i && arr[i].path === path) {
+			let child = {
+				_id: arr[i]._id,
+				name: arr[i].name,
+			}
+
+			if (childrenMap.has(`${path}${child.name}/`)) {
+				child.children = childrenMap.get(`${path}${child.name}/`)
+				childrenMap.delete(`${path}${child.name}/`)
+			}
+
+			children.push(child)
+			i++
+		}
+		childrenMap.set(path, children)
+		arr.splice(0, i)
+	}
+	return childrenMap.get("/")
 }
 
 module.exports = {
-  getGroups,
-  createGroup,
-  getGroupsCustom
+	getGroups,
+	createGroup,
+	getGroupsCustom,
 }
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,38 +1,43 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  groups: {
-    type: [{type : mongoose.Schema.Types.ObjectId, ref : 'Group'}],
-    required: true,
-    default: []
-  },
-  verified : {
-    type : Boolean,
-    required: true,
-    default : false
-  },
-  avatar : {
-    type : mongoose.Schema.Types.ObjectId,
-    ref : 'File',
-    required : false
-  },
-  refreshTokens : [{
-    type : String,
-    required : false
-  }]
-
+	name: {
+		type: String,
+		required: [true, "Name is required"],
+		maxlength: [32, "Name must not exceed 32 characters"],
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		lowercase: true,
+		trim: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	groups: {
+		type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }],
+		required: true,
+		default: [],
+	},
+	verified: {
+		type: Boolean,
+		required: true,
+		default: false,
+	},
+	avatar: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "File",
+		required: false,
+	},
+	refreshTokens: [
+		{
+			type: String,
+			required: false,
+		},
+	],
 })
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema)
