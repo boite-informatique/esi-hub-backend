bd3e32b3be2a38025bc8888248b62ffbb8fecf1e
ricin9 <ricin9@outlook.com>
2022-05-14 18:53:11 +0100

    small change

--- server/middleware/authorize.js
+++ server/middleware/authorize.js
@@ -1,57 +1,61 @@
-const jwt = require('jsonwebtoken')
-const asyncHandler = require('express-async-handler')
-const User = require('../models/userModel')
-const { generateAccessToken } = require('../controllers/userController')
+const jwt = require("jsonwebtoken")
+const asyncHandler = require("express-async-handler")
+const User = require("../models/userModel")
+const { generateAccessToken } = require("../controllers/userController")
 
 module.exports = asyncHandler(async (req, res, next) => {
-  const {accessToken, refreshToken} = req.cookies
+	const { accessToken, refreshToken } = req.cookies
 
-  // check if there is a refresh token
-  if (!refreshToken) {
-    res.status(401)
-    throw new Error('Unauthorized, no refresh token')
-  }
+	// check if there is a refresh token
+	if (!refreshToken) {
+		res.status(401)
+		throw new Error("Unauthorized, no refresh token")
+	}
 
-  let token
-  // check if access token exists and is valid
+	let token
+	// check if access token exists and is valid
 
-  jwt.verify(accessToken, process.env.JWT_SECRET, async (err, decoded) => {
-    if (err) {
+	jwt.verify(accessToken, process.env.JWT_SECRET, async (err, decoded) => {
+		if (err) {
+			const user = await getUser(refreshToken)
 
-    const user = await getUser(refreshToken)
+			if (!user) {
+				res.status(401)
+				throw new Error("Unauthorized, invalid refresh token")
+			}
 
+			token = generateAccessToken(user)
 
-    if (!user) {
-      res.status(401)
-      throw new Error('Unauthorized, invalid refresh token')
-    }
+			res.cookie("accessToken", token, {
+				httpOnly: false,
+				sameSite: true,
+				maxAge: 15 * 60 * 1000,
+			})
 
-    token = generateAccessToken(user)
-
-    res.cookie('accessToken', token, {httpOnly : false, sameSite : true, maxAge : 15 * 60 * 1000})
-
-    req.user = jwt.decode(token)
-    next()
-    } else {
-      req.user = decoded
-      next()
-    }
-  })
+			const { iat, eat, ...userInfo } = jwt.decode(token)
+			req.user = userInfo
+			next()
+		} else {
+			const { iat, eat, ...userInfo } = decoded
+			req.user = userInfo
+			next()
+		}
+	})
 })
 
 const getUser = async (refreshToken) => {
-  // returns user if found and refresh token is valid, null otherwise
-  const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET)
+	// returns user if found and refresh token is valid, null otherwise
+	const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET)
 
-  if (!decoded) {
-   return null 
-  }
+	if (!decoded) {
+		return null
+	}
 
-  const user = await User.findById(decoded.id)
+	const user = await User.findById(decoded.id)
 
-  if (!user) {
-    return null
-  }
+	if (!user) {
+		return null
+	}
 
-  return user.refreshTokens.includes(refreshToken) ? user : null
+	return user.refreshTokens.includes(refreshToken) ? user : null
 }
