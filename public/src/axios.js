c0e567ad864621d99ceffb4928e0ef3a512d1da4
ricin9 <ricin9@outlook.com>
2022-06-12 23:02:10 +0100

    test

--- public/src/axios.js
+++ public/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-	// baseURL: "http://localhost:3005",
-	baseURL: "/",
+	baseURL: "http://localhost:3005",
+	// baseURL: "/",
 	withCredentials: true,
 })
 
